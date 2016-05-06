bf78161d4f4182e31cf1a73833329fc2e8e79e4e
kapil0187 <derivatives@!201>
2016-05-06 15:59:48 -0500

    Generated the structure for the package

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+from pyreuters import __version__, __package__
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+setup(
+    name=__package__,
+    version=__version__,
+    description='Python API To Read Reuters Market Data file',
+    author='Kapil Sharma',
+    author_email='ksharma@dvtrading.co',
+    packages=find_packages(),
+    install_requires=[
+        'pandas>=0.18',
+        'numpy>=1.10',
+        'pytables'
+    ],
+    entry_points={
+        'console_scripts': []
+    },
+    package_data={
+        '': ['*.json']
+    },
+    zip_safe=False
+)
