20a5929c7cf8e21c1b41f49eda6e029ea500fe09
Kapil Sharma <kapil12@outlook.com>
2016-06-12 02:11:11 -0500

    Bare bones #6
    Symbol class definition and methods

---
+++ pyreuters/symbol.py
@@ -0,0 +1,22 @@
+class Symbol(object):
+    def __init__(self):
+        pass
+
+    def load_data(self, start_time, end_time):
+        pass
+
+    def load_data(self, contract, start_time, end_time):
+        pass
+
+    def loaded_contracts(self):
+        pass
+
+    def merge_qt(self, contract):
+        pass
+
+    def merge_qt(self, save=False):
+        pass
+
+    @staticmethod
+    def available():
+        pass
